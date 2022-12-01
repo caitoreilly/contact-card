5dc8068b7d3d3e04f682f24919c12b512fed7c47
Caitlin O'Reilly <coreilly29@gmail.com>
2022-11-30 20:12:41 -0500

    working on rendering indexeddb database data on a card

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,7 +1,6 @@
 //import modules
 
 import "./form";
-import "./submit";
 
 // adding this file --> created an entry point
 // the file that webpack will look at to determine what dependencies and assets
@@ -24,11 +23,54 @@ import { initdb } from "./database";
 // use DOM manipulation to insert images into our page with this code
 window.addEventListener("load", function () {
   initdb();
+  fetchCards();
   document.getElementById("logo").src = Logo;
   document.getElementById("bearThumbnail").src = Bear;
   document.getElementById("dogThumbnail").src = Dog;
 });
 
+// Form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+});
+
 // import Bootstrap's npm modules
 import { Tooltip, Toast, Popper } from "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+// import fetchCards() function from cards.js
+import { fetchCards } from "./cards";
+
+// import toggleForm() and clearForm() from form.js file
+import { toggleForm } from "./form";
+import { clearForm } from "./form";
